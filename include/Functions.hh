4d052d89a1694b210f05598a91da9acb6db864da
moon1523 <moonsungho@hanyang.ac.kr>
2023-10-30 13:07:03 +0900

    view

--- include/Functions.hh
+++ include/Functions.hh
@@ -28,7 +28,6 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/viz.hpp>
 #include <opencv2/core/eigen.hpp>
 
 #include <sl/Camera.hpp>
--- include/ViewCoords.hh
+++ include/ViewCoords.hh
@@ -5,6 +5,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/viz.hpp>
 
 #include <iostream>
 #include <fstream>
